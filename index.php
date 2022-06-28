a2afffed0eb05bebe92902d80e05795a50c1b143
Dinko <dinko.dugec@gmail.com>
2022-06-29 00:46:58 +0200

    Get the controller and action from a URL with a variable structure

--- index.php
+++ index.php
@@ -3,10 +3,8 @@
 /**
  * Front controller
  *
- * 
- */
 
-// echo 'Requested URL = "' . $_SERVER['QUERY_STRING'] . '"';
+ */
 
 /**
  * Routing
@@ -15,18 +13,21 @@ require 'Core/Router.php';
 
 $router = new Router();
 
-//echo get_class($router);
-
 // Add the routes
 $router->add('', ['controller' => 'Home', 'action' => 'index']);
 $router->add('posts', ['controller' => 'Posts', 'action' => 'index']);
-$router->add('posts/new', ['controller' => 'Posts', 'action' => 'new']);
+//$router->add('posts/new', ['controller' => 'Posts', 'action' => 'new']);
+$router->add('{controller}/{action}');
+$router->add('admin/{action}/{controller}');
     
-/* // Display the routing table
+// Display the routing table
 echo '<pre>';
-var_dump($router->getRoutes());
-echo '</pre>'; */
+//var_dump($router->getRoutes());
+echo htmlspecialchars(print_r($router->getRoutes(), true));
+echo '</pre>';
 
+
+// Match the requested route
 $url = $_SERVER['QUERY_STRING'];
 
 if ($router->match($url)) {
@@ -36,4 +37,3 @@ if ($router->match($url)) {
 } else {
     echo "No route found for URL '$url'";
 }
-
