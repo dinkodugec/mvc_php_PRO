09aa5b93a4867064a6c506e71e2d611567f53674
Dinko <dinko.dugec@gmail.com>
2022-06-29 19:33:32 +0200

    creating controller object and run action method

--- index.php
+++ index.php
@@ -3,8 +3,11 @@
 /**
  * Front controller
  *
-
+ * PHP version 5.4
  */
+
+// Require the controller class
+require 'App/Controllers/Posts.php';
 
 /**
  * Routing
@@ -15,11 +18,10 @@ $router = new Router();
 
 // Add the routes
 $router->add('', ['controller' => 'Home', 'action' => 'index']);
-$router->add('posts', ['controller' => 'Posts', 'action' => 'index']);
-//$router->add('posts/new', ['controller' => 'Posts', 'action' => 'new']);
 $router->add('{controller}/{action}');
-$router->add('admin/{action}/{controller}');
+$router->add('{controller}/{id:\d+}/{action}');
     
+/*
 // Display the routing table
 echo '<pre>';
 //var_dump($router->getRoutes());
@@ -37,3 +39,5 @@ if ($router->match($url)) {
 } else {
     echo "No route found for URL '$url'";
 }
+*/
+$router->dispatch($_SERVER['QUERY_STRING']);
