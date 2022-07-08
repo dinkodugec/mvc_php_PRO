10afed6a84c95f86dbc736feafa51f1a64f69761
Dinko <dinko.dugec@gmail.com>
2022-07-08 23:15:42 +0200

    Put application configuration settings in a separate file

---
+++ App/Config.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App;
+
+/**
+ * Application configuration
+ *
+ * 
+ */
+class Config
+{
+
+    /**
+     * Database host
+     * @var string
+     */
+    const DB_HOST = 'localhost';
+
+    /**
+     * Database name
+     * @var string
+     */
+    const DB_NAME = 'mvc_pro';
+
+    /**
+     * Database user
+     * @var string
+     */
+    const DB_USER = 'root';
+
+    /**
+     * Database password
+     * @var string
+     */
+    const DB_PASSWORD = 'secret';
+}
